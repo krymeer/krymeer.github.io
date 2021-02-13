322083ebb59d3c60d54039256e83e0495ae35de9
krymeer <krzysztof.radoslaw.osada@gmail.com>
2021-02-13 23:14:21 +0100

    Some joy with hashes

--- src/jsx/scripts.jsx
+++ src/jsx/scripts.jsx
@@ -11,6 +11,12 @@ class Header extends React.Component {
         return (
             <header>
                 <h1>Hello World!</h1>
+                <nav>
+                    <a href="#home">Home</a>
+                    <a href="#about-me">About me</a>
+                    <a href="#works">Works</a>
+                    <a href="#hobbies">Hobbies</a>
+                </nav>
             </header>
         );
     }
@@ -28,6 +34,7 @@ class Main extends React.Component {
             <main>
                 <p>This page is only a stub, yet it is going to be my home page in the future.</p>
                 <p><span className="text-blue">Stay tuned.</span> Nifty stuff will come soon.</p>
+                <p>The page you are looking at now is <span className="text-green">{ this.props.pageName }</span>.</p>
             </main>
         );
     }
@@ -54,14 +61,59 @@ class App extends React.Component {
     constructor( props )
     {
         super( props );
+        this.handleHashChange = this.handleHashChange.bind( this );
+        this.state            = {
+            pageName : this.getPageName()
+        }
+    }
+
+    componentDidMount()
+    {
+        window.addEventListener( 'hashchange', this.handleHashChange );
+    }
+
+    componentWillUnmount()
+    {
+        window.removeEventListener( 'hashchange', this.handleHashChange );
+    }
+
+    getPageName()
+    {
+        let pageName;
+
+        switch( window.location.hash.substring( 1 ) )
+        {
+            case 'about-me':
+                pageName = 'About me';
+                break;
+            case 'works':
+                pageName = 'Works';
+                break;
+            case 'hobbies':
+                pageName = 'Hobbies';
+                break;
+            case 'home':
+            default:
+                pageName = 'Home';
+        }
+
+        return pageName;
+    }
+
+    handleHashChange()
+    {
+        this.setState( {
+            pageName : this.getPageName()
+        } );
     }
 
     render()
     {
+
         return (
-            <div class="container">
+            <div className="container">
                 <Header />
-                <Main />
+                <Main pageName={ this.state.pageName } />
                 <Footer />
             </div>
         );
