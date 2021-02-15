00bc4341dbb4a4452f5a4d2dc683deaa6bd4eefd
krymeer <krzysztof.radoslaw.osada@gmail.com>
2021-02-15 21:54:01 +0100

    Building mini pages from scratch

--- src/jsx/scripts.jsx
+++ src/jsx/scripts.jsx
@@ -8,15 +8,14 @@ class Header extends React.Component {
 
     render()
     {
+        const navItems = this.props.pages.map( ( page, pageIndex ) =>
+            <a className={ 'nav__item' + ( ( this.props.page.slug && page.slug === this.props.page.slug ) ? ' nav__item--current' : '' ) } key={ pageIndex } href={ '#' + page.slug }>{ page.title }</a>
+        );
+
         return (
             <header className="wrapper__item">
                 <h1 className="head mb-05">Hello World!</h1>
-                <nav className="nav">
-                    <a className="nav__item" href="#home">Home</a>
-                    <a className="nav__item" href="#about-me">About me</a>
-                    <a className="nav__item" href="#works">Works</a>
-                    <a className="nav__item" href="#hobbies">Hobbies</a>
-                </nav>
+                <nav className="nav">{ navItems }</nav>
             </header>
         );
     }
@@ -30,12 +29,13 @@ class Main extends React.Component {
 
     render()
     {
+        if( !this.props.page || !this.props.page.content )
+        {
+            return null;
+        }
+
         return (
-            <main className="wrapper__item">
-                <p className="para mb-10">This page is only a stub, yet it is going to be my home page in the future.</p>
-                <p className="para mb-10"><span className="text--blue">Stay tuned.</span> Nifty stuff will come soon.</p>
-                <p className="para">The page you are looking at now is <span className="text--green">{ this.props.pageName }</span>.</p>
-            </main>
+            <main className="wrapper__item">{ this.props.page.content }</main>
         );
     }
 }
@@ -62,8 +62,41 @@ class App extends React.Component {
     {
         super( props );
         this.handleHashChange = this.handleHashChange.bind( this );
+        this.pages            = [
+            {
+                slug    : 'home',
+                title   : 'Home',
+                content : <React.Fragment>
+                    <p className="para mb-10">This page is only a stub, yet it is going to be my home page in the future.</p>
+                    <p className="para"><span className="text--blue">Stay tuned.</span> Nifty stuff will come soon.</p>
+                </React.Fragment>
+            },
+            {
+                slug    : 'about-me',
+                title   : 'About me',
+                content : <React.Fragment>
+                    <p className="para"><span className="text--green">Web developer</span> based in Wrocław, Poland.</p>
+                    <p className="para">To be continued.</p>
+                </React.Fragment>
+            },
+            {
+                slug    : 'works',
+                title   : 'Works',
+                content : <React.Fragment>
+                    <p className="para mb-10">I have been working at <a className="link" href="https://adagri.com/" target="_blank">Adagri</a> since 2019.</p>
+                    <p className="para">You could find some of my personal projects on my <a className="link" href="https://github.com/krymeer" target="_blank">GitHub repo</a>.</p>
+                </React.Fragment>
+            },
+            {
+                slug    : 'hobbies', 
+                title   : 'Hobbies',
+                content : <React.Fragment>
+                    <p className="para">Half-marathoner, blood donor, genealogist, pastry chef and traveller. </p>
+                </React.Fragment>
+            }
+        ];
         this.state            = {
-            pageName : this.getPageName()
+            page : this.getPage()
         }
     }
 
@@ -77,43 +110,28 @@ class App extends React.Component {
         window.removeEventListener( 'hashchange', this.handleHashChange );
     }
 
-    getPageName()
+    getPage()
     {
-        let pageName;
+        const thisSlug = window.location.hash.substring( 1 );
+        let thisPage   = this.pages.find( page => ( !thisSlug && page.slug === 'home' ) || thisSlug === page.slug );
+        document.title = thisPage.title + ' - ' + document.title.replace( /^[^\-]+- /, '' );
 
-        switch( window.location.hash.substring( 1 ) )
-        {
-            case 'about-me':
-                pageName = 'About me';
-                break;
-            case 'works':
-                pageName = 'Works';
-                break;
-            case 'hobbies':
-                pageName = 'Hobbies';
-                break;
-            case 'home':
-            default:
-                pageName = 'Home';
-        }
-
-        return pageName;
+        return thisPage;
     }
 
     handleHashChange()
     {
         this.setState( {
-            pageName : this.getPageName()
+            page : this.getPage()
         } );
     }
 
     render()
     {
-
         return (
             <div className="wrapper wrapper--inner">
-                <Header />
-                <Main pageName={ this.state.pageName } />
+                <Header pages={ this.pages } page={ this.state.page } />
+                <Main page={ this.state.page } />
                 <Footer />
             </div>
         );
